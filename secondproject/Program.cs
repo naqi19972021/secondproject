1dc188ddb1864fdf1e0004bdb609886898e3b2e2
naqi19972021 <naqi20164@hotmail.com>
2021-10-03 14:00:28 +0100

    modified hello worled to hello first chanhge

--- secondproject/Program.cs
+++ secondproject/Program.cs
@@ -6,7 +6,7 @@ namespace secondproject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello first change!");
         }
     }
 }
